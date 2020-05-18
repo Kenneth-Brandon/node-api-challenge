5a36fc867ae320eb9d1e1c8631e39442df7c0169
Kenneth-Brandon <onegiantsfan25@yahoo.com>
2020-05-17 19:16:21 -0700

    added custom middleware validateProject and validateAction

--- server/middleware/middleware.js
+++ server/middleware/middleware.js
@@ -6,6 +6,58 @@ const logger = (request, response, next) => {
   next();
 };
 
+const validateId = (db) => (request, response, next) => {
+  const { id } = request.params;
+  console.log(params);
+
+  db.get(id)
+    .then((item) => {
+      item !== null
+        ? (request.item = item)
+        : response.status(400).json({ message: 'Invalid id' });
+      next();
+    })
+    .catch((error) => {
+      response.status(500).json({ errorMessage: "Couldn't validate", error });
+    });
+};
+
+const validateProject = (request, response, next) => {
+  const { body } = request;
+
+  JSON.stringify(body) === '{}'
+    ? response.status(400).json({ message: 'Missing project data' })
+    : !body.name || !body.description
+    ? response.status(400).json({
+        message: `missing required 
+        ${
+          !body.name ? 'name' : !body.description ? 'description' : null
+        } field`,
+      })
+    : next();
+};
+
+const validateAction = (request, response, next) => {
+  const { body } = request;
+
+  JSON.stringify(body) === '{}'
+    ? response.status(400).json({ message: 'missing action data' })
+    : !body.project_id
+    ? response.status(400).json({ message: 'missing required project_id' })
+    : !body.description || !body.notes
+    ? response.status(400).json({
+        message: `missing required 
+          ${
+            !body.description ? 'description' : !body.notes ? 'notes' : null
+          } field`,
+      })
+    : !body.description.length <= 128
+    ? response
+        .status(400)
+        .json({ message: 'description must be 128 characters or fewer' })
+    : next();
+};
+
 const middleware = [logger];
 
-module.exports = middleware;
+module.exports = { middleware, validateId, validateProject, validateAction };
--- server/projects/projectRouter.js
+++ server/projects/projectRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const projectDb = require('../../data/helpers/projectModel');
+const { validateId } = require('../middleware/middleware');
 
 const router = express.Router();
 
@@ -10,6 +11,11 @@ router.get('/', (request, response) => {
   projectDb.get().then((projects) => {
     response.status(200).json(projects);
   });
+});
+// tested
+
+router.get('/:id', validateId(projectDb), (request, response) => {
+  response.status(200).json(request.item);
 });
 
 module.exports = router;
